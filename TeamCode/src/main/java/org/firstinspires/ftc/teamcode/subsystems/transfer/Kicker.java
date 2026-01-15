7dbe898c6a070397d8b40734ae62b4e314e6242c
athGurnani <atharvgurnani@gmail.com>
2026-01-14 21:25:06 -0600

    Try to turn kicker to Mercurial

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/transfer/Kicker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/transfer/Kicker.java
@@ -1,25 +1,70 @@
 package org.firstinspires.ftc.teamcode.subsystems.transfer;
 
-import com.qualcomm.robotcore.hardware.HardwareMap;
+import androidx.annotation.NonNull;
+
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.util.Settings;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.util.concurrent.CompletableFuture;
 
+import dev.frozenmilk.dairy.core.FeatureRegistrar;
+import dev.frozenmilk.dairy.core.dependency.Dependency;
+import dev.frozenmilk.dairy.core.dependency.annotation.SingleAnnotation;
+import dev.frozenmilk.dairy.core.wrapper.Wrapper;
+import dev.frozenmilk.mercurial.commands.Lambda;
+import dev.frozenmilk.mercurial.subsystems.Subsystem;
+import dev.frozenmilk.mercurial.subsystems.SubsystemObjectCell;
+import kotlin.annotation.MustBeDocumented;
+
 @SuppressWarnings("all")
-public class Kicker {
-    private Servo[] kickers;
+public class Kicker implements Subsystem {
+    public static final Kicker INSTANCE = new Kicker() {};
+    private SubsystemObjectCell<Servo[]> kickers;
+    public static Servo[] getServos() {
+        return INSTANCE.kickers.get();
+    }
     private ElapsedTime servoTimer;
     private Integer[] order;
     private CompletableFuture<Void> runFireSequence = null;
 
-    public Kicker(HardwareMap hwMap) {
-            kickers = new Servo[3];
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @MustBeDocumented
+    @Inherited
+    public @interface Attach{}
 
-            for (int i = 0; i < kickers.length; i++) {
-                kickers[i] = hwMap.get(Servo.class, Settings.HardwareNames.Transfer.KICKERS[i]);
+    private Dependency<?> dependency =
+            // the default dependency ensures that mercurial is attached
+            Subsystem.DEFAULT_DEPENDENCY
+                    // this is the standard attach annotation that is recommended for features
+                    // if you are using other features, you should add them as
+                    // dependencies as well
+                    // you can also use the annotation to set up and manage
+                    // declarative settings for your subsystem, if desired
+                    .and(new SingleAnnotation<>(Attach.class));
+
+
+
+    // init code might go in here
+    @Override
+    public void preUserInitHook(@NonNull Wrapper opMode) {
+        // default command should be set up here, not in the constructor
+        setDefaultCommand(/*simpleCommand()*/null);
+    }
+
+    /**
+     * TODO: Swap to Mercurial 2.0
+     */
+    public Kicker() {
+            for (int i = 0; i < kickers.get().length; i++) {
+                subsystemCell(() -> FeatureRegistrar.getActiveOpMode().hardwareMap.get(Servo.class, Settings.HardwareNames.Transfer.KICKERS[0]));
             }
 
             servoTimer = new ElapsedTime(ElapsedTime.Resolution.SECONDS);
@@ -60,20 +105,45 @@ public class Kicker {
     }
 
     public void kickServoUp(int servoIndex) {
-        kickers[servoIndex].setPosition(Settings.Positions.Transfer.upPos[servoIndex]);
+        kickers.get()[servoIndex].setPosition(Settings.Positions.Transfer.upPos[servoIndex]);
     }
 
     public void kickServoDown(int servoIndex) {
-        kickers[servoIndex].setPosition(Settings.Positions.Transfer.downPos[servoIndex]);
+        kickers.get()[servoIndex].setPosition(Settings.Positions.Transfer.downPos[servoIndex]);
     }
 
     public void kickAllServosDown() {
-        for (int i = 0; i < kickers.length; i++) {
+        for (int i = 0; i < kickers.get().length; i++) {
             this.kickServoDown(i);
         }
     }
 
     public Double[] getServoPositions() {
-        return new Double[] {kickers[0].getPosition(), kickers[1].getPosition(), kickers[2].getPosition()};
+        return new Double[] {kickers.get()[0].getPosition(), kickers.get()[1].getPosition(), kickers.get()[2].getPosition()};
+    }
+
+    @NonNull
+    @Override
+    public Dependency<?> getDependency() {
+        return dependency;
+    }
+
+    @Override
+    public void setDependency(@NonNull Dependency<?> dependency) {
+        this.dependency = dependency;
+    }
+
+    @NonNull
+    public static Lambda simpleCommand() {
+        // we need to give commands names
+        // names help to give helpful error messages when something goes wrong in your command
+        // Mercurial will automatically rename your command to match the standard convention
+        // learn more about names and error messages in the names and messages overview
+        return new Lambda("simple")
+                .addRequirements(INSTANCE)
+                .setInit(() -> INSTANCE.createFireSequence())
+                .setEnd(interrupted -> {
+                    if (!interrupted) INSTANCE.kickAllServosDown();
+                });
     }
 }
