531d4623798098c7fb900aed7e0db2d76015c539
athGurnani <atharvgurnani@gmail.com>
2026-01-17 20:48:06 -0600

    Simplify Controller initialization and memory management

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Controller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Controller.java
@@ -1,28 +1,23 @@
 package org.firstinspires.ftc.teamcode.util;
 
-import com.qualcomm.robotcore.hardware.Gamepad;
-
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.function.BooleanSupplier;
 import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
 
+@SuppressWarnings("all")
 public class Controller {
-    private Gamepad gamepad;
     private ArrayList<BooleanSupplier> criterias = new ArrayList<>();
     private ArrayList<Runnable> actions = new ArrayList<>();
     private ArrayList<Object> actionParameters = new ArrayList<>();
-
-    public Controller(Gamepad gamepad) {
-        this.gamepad = gamepad;
-    }
 
     public <T> void bindAction(BooleanSupplier condition, T actionParameter, Consumer<T> action) {
         this.criterias.add(condition);
         this.actions.add(() -> action.accept(actionParameter));
         this.actionParameters.add(actionParameter);
+    }
+    public void start() {
+        actionParameters.clear();
+        actionParameters = null;
     }
 
     public void update() {
