983f39466f2e259248416f1bde8f4143c74a03e5
athGurnani <atharvgurnani@gmail.com>
2026-02-07 11:49:55 -0600

    Add Kotlin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/KotlinTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/KotlinTest.kt
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.teamcode.tests
 
-class KotlinTest {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.teamcode.subsystems.Intake
+
+@TeleOp(name="KOTLIN :)")
+class KotlinTest : OpMode() {
+    var intake: Intake = Intake(hardwareMap)
+
+    override fun init (){
+        telemetry.addLine("IT WORKS")
+        telemetry.update()
+    }
+
+    override fun init_loop() {
+        if (gamepad1.a) intake.run()
+        else intake.stop()
+    }
+    override fun loop() {
+
+    }
 }
