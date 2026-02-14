71fdb4e04e1f305d724929cf978190dda585655c
athGurnani <atharvgurnani@gmail.com>
2026-02-14 10:50:24 -0600

    Simplify hardware control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/BlueFar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/BlueFar.java
@@ -11,6 +11,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.util.AllianceColor;
 import org.firstinspires.ftc.teamcode.util.Artifact;
+import org.firstinspires.ftc.teamcode.util.Hardware;
 import org.firstinspires.ftc.teamcode.util.MatchSettings;
 
 import java.util.List;
@@ -25,19 +26,12 @@ public class BlueFar extends OpMode {
         MatchSettings.initSelection(hardwareMap, new AllianceColor(AllianceColor.Selection.BLUE), gamepad1);
         MatchSettings.start();
         dt.startTeleOpDrive();
-
-        for (LynxModule module : allHubs) {
-            module.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
-        }
     }
 
     @Override
     public void loop() {
-        for (LynxModule module : allHubs) {
-            module.getBulkData();
-        }
+        Hardware.loop();
 
-        dt.update();
         intake.run();
         shooter.turret.loop(
                 dt.follower.getPose().getX(),
@@ -53,13 +47,12 @@ public class BlueFar extends OpMode {
                 -gamepad1.left_stick_x,
                 -gamepad1.right_stick_x
         );
-        if (gamepad1.aWasPressed()) {
-            /*transfer.kicker.setFireSequence(
-                    transfer.sorter.getOrder()
-            );
-            transfer.kicker.createFireSequence();*/
-            transfer.fireSortedArtifacts();
 
+        if (gamepad1.aWasPressed()) {
+            transfer.fireSortedArtifacts();
+        }
+        else if (gamepad1.bWasPressed()) {
+            transfer.cancelFire();
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Hardware.java
@@ -4,6 +4,7 @@ import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.LynxHubs;
 import org.firstinspires.ftc.teamcode.subsystems.drivetrain.Drivetrain;
 import org.firstinspires.ftc.teamcode.subsystems.shooter.Shooter;
 import org.firstinspires.ftc.teamcode.subsystems.transfer.Transfer;
@@ -13,6 +14,7 @@ public class Hardware {
     public static Transfer transfer;
     public static Shooter shooter;
     public static Intake intake;
+    public static LynxHubs hubs;
     public static void initialize(HardwareMap hwMap, AllianceColor alliance, Pose gamepadReference) {
         dt = new Drivetrain(hwMap, alliance,gamepadReference, new Pose(1.15, 1.15, 1.15));
         dt.follower.setStartingPose(
@@ -26,5 +28,19 @@ public class Hardware {
         shooter = new Shooter(hwMap);
         shooter.setAlliance(alliance);
         intake = new Intake(hwMap);
+        hubs = new LynxHubs();
+        hubs.init(hwMap, alliance);
+    }
+
+    public static void loop() {
+        dt.update();
+        hubs.loop();
+    }
+
+    public static void stop() {
+        transfer.kicker.stop();
+        hubs.stop();
+        intake.stop();
+
     }
 }
