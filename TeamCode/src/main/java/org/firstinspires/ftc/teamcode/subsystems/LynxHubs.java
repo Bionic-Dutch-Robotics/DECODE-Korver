d090426d04a557150e28e0e9b46fc15a3e6dc525
athGurnani <atharvgurnani@gmail.com>
2026-03-08 17:14:09 -0500

    Add i2c sensor reading

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LynxHubs.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LynxHubs.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
 
 import org.firstinspires.ftc.teamcode.util.AllianceColor;
 
@@ -30,9 +31,33 @@ public class LynxHubs extends Subsystem {
         ctrlHubData = allHubs.get(0).getBulkData();
         exHubData = allHubs.get(1).getBulkData();
 
-        //TODO: Figure out how to get/set motor/servo/sensor values
+        // Servos cannot be read
+    }
+
+    public double getMotorPosition(Hub hub, int port) {
+        return hub == Hub.CONTROL_HUB ? ctrlHubData.getMotorCurrentPosition(port)
+                : exHubData.getMotorCurrentPosition(port);
+    }
+
+    public double getAnalogInputVoltage(Hub hub, int port) {
+        return hub == Hub.CONTROL_HUB ? ctrlHubData.getAnalogInputVoltage(port) :
+                exHubData.getAnalogInputVoltage(port);
+    }
+
+    public byte[] getSensorData(I2cDevice sensor) {
+        return sensor.device.read(0x08, 3);
     }
 
     @Override
     public void stop() {allHubs = null;}
+
+    public enum Hub {
+        CONTROL_HUB,
+        EXPANSION_HUB
+    }
+
+    public class I2cDevice {
+        I2cDeviceSynch device;
+        String name;
+    }
 }
